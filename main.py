831574b058ea785eb8789ca2eaf12e4dd3b9dd32
Diana <diana210700@gmail.com>
2022-05-28 17:16:15 +0300

    Change password length

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+    password = db.Column(db.String(1000))
     name = db.Column(db.String(1000))
     # This will act like a List of BlogPost objects attached to each User.
     # The "author" refers to the author property in the BlogPost class.
